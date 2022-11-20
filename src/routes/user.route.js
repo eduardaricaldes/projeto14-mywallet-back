bcfcc3446cb3b13f5b754a246bf91b50474011cc
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-11-20 18:02:18 -0300

    modificado para post na rota signup

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,7 @@ import { users, signIn } from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.get("/sign-up", users)
+router.post("/sign-up", users)
 router.post("/sign-in", signIn);
 
 export default router;
